7230ce87846bbddf76a0b8891dc6e5cfe9c0f70b
a-turker-synnada <abdussamet.turker@synnada.ai>
2024-11-07 10:08:03 +0300

    realse 3

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ with open("README.md") as f:
 
 setuptools.setup(
     name="sarmal",
-    version="0.2.15",
+    version="0.3.0",
     author="Synnada, Inc.",
     author_email="opensource@synnada.ai",
     description="A Modular Machine Learning Library for Model Composability",
