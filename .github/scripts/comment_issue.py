02a8b5cf447db665944e514b1f481a9cfd2bef65
mehmetozsoy1 <146070048+mehmetozsoy1@users.noreply.github.com>
2024-11-07 15:45:52 +0300

    Update comment_issue.py

--- .github/scripts/comment_issue.py
+++ .github/scripts/comment_issue.py
@@ -1,30 +1,37 @@
 import sys
-import yaml # type[ignore]
+import yaml # type: ignore
 import subprocess
 
 # Load labels from GitHub Action input
-labels = sys.argv[1].split(',')
-number = sys.argv[2]
-body = sys.argv[3]
+body = sys.argv[1]
+label = sys.argv[2].split(',')
+number = sys.argv[3]
 
 # Load the label-user mapping from the YAML file
 with open('.github/label_to_devs.yml', 'r') as file:
     label_user_mapping = yaml.safe_load(file)
 
-print("Body:", body)
-print()
-print(labels)
-print()
-print(number)
-# Find reviewers based on labels
-# for label in labels:
-#     reviewers = set()
-#     label = label.strip()
-#     if label in label_user_mapping:
-#         reviewers.update(label_user_mapping[label])
-#     if reviewers:
-#         prefix_arg = "gh pr edit --add-assignee "
-#         reviewers_arg = "".join(f"{reviewer} " for reviewer in reviewers)
-#         arg = prefix_arg + reviewers_arg
-#         subprocess.run(arg, shell=True)
-            
+
+if (related_users := label_user_mapping.get(label)):
+    related_users = set(related_users)
+    lines = body.split("\n")
+    
+    last_line = lines[-1]
+    # check if the issue already has a CC
+    if last_line[:4] == "CC: ":
+        lines.pop()
+        users = last_line[4:].split(", ") # find all users in the CC
+        users = [user[1:]  for user in users] # remove the @
+        all_users = set(users) | related_users # cobine the old and new users
+    else:
+        # add three lines for formatting
+        lines.append("")
+        lines.append("")
+        lines.append("")
+        all_users = related_users
+    
+    new_line = "CC: " + "".join(f"@{user}, " for user in all_users)
+    lines[-1] = new_line
+    new_body = "".join(f"{line}\n" for line in lines)
+    command = f"gh issue edit {number} --body '{new_body}'"
+    subprocess.run(command, shell=True)
